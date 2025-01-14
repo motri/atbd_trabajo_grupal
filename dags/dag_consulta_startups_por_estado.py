9564cc0a066bfdf5125c20f38f6286ab8b41cf70
Unai Motriko <unaimotriko@gmail.com>
2025-01-14 11:45:32 +0100

    Add files via upload

---
+++ dags/dag_consulta_startups_por_estado.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from datetime import datetime
+
+with DAG(
+    dag_id='lanza_consulta_startups_fundadas_por_estado',
+    start_date=datetime(2025, 1, 1),
+    schedule_interval=None,  # Trigger manually
+    catchup=False,
+    tags=['atbd_consultas'],
+) as dag:
+
+    # Spark submit task
+    spark_submit_task = SparkSubmitOperator(
+        task_id='ejecuta_consulta',
+        application='hdfs://172.31.20.226:9000/user/ec2-user/scripts/new_startups_by_state.py',
+        conn_id='spark_default',  # Airflow connection ID for Spark
+        name='consulta_startups_fundadas_por_estado',
+        verbose=True,
+        conf={
+            'spark.executor.memory': '1g',
+            'spark.executor.cores': '1',
+            'spark.yarn.archive': 'hdfs://172.31.20.226:9000/user/ec2-user/spark-hadoop-libs.zip',
+        },
+        env_vars={
+            'HADOOP_CONF_DIR': '/etc/hadoop/conf',
+            'YARN_CONF_DIR': '/etc/hadoop/conf',
+        },
+    )
+
+    spark_submit_task
---
+++ dags/dag_inversiones_publicas_en_i+d.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from datetime import datetime
+
+with DAG(
+    dag_id='lanza_consulta_inversiones_publicas_en_i+d',
+    start_date=datetime(2025, 1, 1),
+    schedule_interval=None,  # Trigger manually
+    catchup=False,
+    tags=['atbd_consultas'],
+) as dag:
+
+    # Spark submit task
+    spark_submit_task = SparkSubmitOperator(
+        task_id='ejecuta_consulta',
+        application='hdfs://172.31.20.226:9000/user/ec2-user/scripts/total_award_vs_rnd_multitype.py',
+        conn_id='spark_default',  # Airflow connection ID for Spark
+        name='consulta_inversiones_publicas_en_i+d',
+        verbose=True,
+        conf={
+            'spark.executor.memory': '1g',
+            'spark.executor.cores': '1',
+            'spark.yarn.archive': 'hdfs://172.31.20.226:9000/user/ec2-user/spark-hadoop-libs.zip',
+        },
+        env_vars={
+            'HADOOP_CONF_DIR': '/etc/hadoop/conf',
+            'YARN_CONF_DIR': '/etc/hadoop/conf',
+        },
+    )
+
+    spark_submit_task
