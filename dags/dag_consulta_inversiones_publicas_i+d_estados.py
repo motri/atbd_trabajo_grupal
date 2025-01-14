ac9cf746a6ea853ef88358dcb0c837af8570bb40
Unai Motriko <unaimotriko@gmail.com>
2025-01-14 12:03:55 +0100

    Update dag_consulta_inversiones_publicas_i+d_estados.py

--- dags/dag_consulta_inversiones_publicas_i+d_estados.py
+++ dags/dag_consulta_inversiones_publicas_i+d_estados.py
@@ -3,9 +3,9 @@ from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOpe
 from datetime import datetime
 
 with DAG(
-    dag_id='lanza_consulta_consulta_startups_en_hubs_universitarios',
+    dag_id='lanza_consulta_inversiones_publicas_i+d_estados',
     start_date=datetime(2025, 1, 1),
-    schedule_interval=None,  # Trigger manually
+    schedule_interval=None,  
     catchup=False,
     tags=['atbd_consultas'],
 ) as dag:
@@ -14,8 +14,8 @@ with DAG(
     spark_submit_task = SparkSubmitOperator(
         task_id='ejecuta_consulta',
         application='hdfs://172.31.20.226:9000/user/ec2-user/scripts/rnd_state_changes.py',
-        conn_id='spark_default',  # Airflow connection ID for Spark
-        name='consulta_consulta_startups_en_hubs_universitarios',
+        conn_id='spark_default', 
+        name='consulta_inversiones_publicas_i+d_estados',
         verbose=True,
         conf={
             'spark.executor.memory': '1g',
